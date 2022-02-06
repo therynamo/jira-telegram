8c7ba7c94d78663c860249a4a25e6ab3089299c6
therynamo <theryn.groetken@gemini.com>
2022-02-06 15:40:29 -0600

    chore: test

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {context} from '@actions/github';
 async function run(): Promise<void> {
   try {
     // eslint-disable-next-line no-console
-    console.log(`Hi ${JSON.stringify(context.payload, null, 2)}`);
+    console.log(`${JSON.stringify(context.payload, null, 2)}`);
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
